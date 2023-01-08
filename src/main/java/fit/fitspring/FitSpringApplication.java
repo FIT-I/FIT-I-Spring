a0bfc07e466fc447372e45a4af17117eadcd4d43
jooomOut <Per@gimjungiui-MacBookPro.local>
2023-01-08 21:55:48 +0900

    initial commit

---
+++ src/main/java/fit/fitspring/FitSpringApplication.java
@@ -0,0 +1,13 @@
+package fit.fitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fit/fitspring/FitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.fitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
