13e906ce4628cf9fee964631b9fd3972e137f597
SY2on <sjk8w7@gmail.com>
2023-01-19 23:55:44 +0900

    Add: Entity추가 및 수정

---
+++ src/main/java/fit/fitspring/config/Secret.java
@@ -0,0 +1,6 @@
+package fit.fitspring.config;
+
+public class Secret {
+    public static String JWT_SECRET_KEY = "ekjwjdksjflejlFEIJdkjfiwjskEwkdfkwnfea-.eskefk-125s4ejfiwoaJWT";
+    public static String USER_INFO_PASSWORD_KEY = "aoisjfo.eij33owaAEwi-efaSi*fhahWEIhfowfA839QH.SIAFHOw3asjhfejwt";
+}
