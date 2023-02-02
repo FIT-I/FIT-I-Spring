e05580561dbfa6e15b7e1375614c83f677641922
unknown <caujisu2020@gmail.com>
2023-02-02 20:14:09 +0900

    Feat: 고객 회원가입 validation api 추가

---
+++ src/main/java/fit/fitspring/controller/dto/account/RegisterCustomerValidationDto.java
@@ -0,0 +1,40 @@
+package fit.fitspring.controller.dto.account;
+
+import fit.fitspring.domain.account.Account;
+import fit.fitspring.domain.account.AccountType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@Schema(title = "회원정보")
+public class RegisterCustomerValidationDto {
+    @Schema(description = "이름", example = "홍길동")
+    private String name;
+    @Schema(description = "이메일", example = "fiti@soongsil.ac.kr")
+    private String email;
+    @Schema(description = "비밀번호", example = "fiti123!")
+    private String password;
+
+
+    public RegisterCustomerValidationDto(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Account toEntity(){
+        return Account.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .profile("img_test")
+                .accountType(AccountType.CUSTOMER)
+                .build();
+    }
+}
