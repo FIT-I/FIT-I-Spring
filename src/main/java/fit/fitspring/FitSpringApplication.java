3549d69655706fbdf137bbfa78c67f28953dc582
isExample <yeji0331@naver.com>
2023-02-04 16:23:29 +0900

    Fix: Swagger server url에 https 반영

--- src/main/java/fit/fitspring/FitSpringApplication.java
+++ src/main/java/fit/fitspring/FitSpringApplication.java
@@ -1,11 +1,21 @@
 package fit.fitspring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+        @Server(url = "${custom.http.server}", description = "Generated server url"),
+        @Server(url = "${custom.https.server}", description = "Security Verification server url"),
+        @Server(url = "${custom.localhost.server}"+"${server.port}", description = "Generated server url"),
+})
 public class FitSpringApplication {
 
     public static void main(String[] args) {
