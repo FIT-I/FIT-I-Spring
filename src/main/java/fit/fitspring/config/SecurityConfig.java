887fa83cc2261a2b4f572bab010b5eaad352cfc1
unknown <caujisu2020@gmail.com>
2023-02-09 06:31:05 +0900

    Fix: 불필요한 선언 삭제

--- src/main/java/fit/fitspring/config/SecurityConfig.java
+++ src/main/java/fit/fitspring/config/SecurityConfig.java
@@ -9,11 +9,9 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -88,11 +86,4 @@ public class SecurityConfig {
 
         return httpSecurity.build();
     }
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer(){
-//        return web -> {
-//            web.ignoring()
-//        };
-//    }
 }
